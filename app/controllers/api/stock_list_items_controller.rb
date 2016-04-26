03d26f186c6d5af7fac2fe0c09df352d80b3ee97
Gage <gagesnewman@gmail.com>
2016-04-26 15:12:00 -0700

    some logic done for stock_list_items

--- app/controllers/api/stock_list_items_controller.rb
+++ app/controllers/api/stock_list_items_controller.rb
@@ -1,7 +1,10 @@
 class Api::StockListItemsController < ApplicationController
   def create
     @stock_list_item = StockListItem.new(stock_list_item_params)
+    @stock_list_item.list_id = List.find(user_id: current_user.id).id
+
     if @stock_list_item.save
+      set_list_tail!
       status = 200
       render 'api/lists/show', status: status
     else
@@ -13,5 +16,27 @@ class Api::StockListItemsController < ApplicationController
   def update
   end
   def destroy
+  end
+
+  private
+  def stock_list_item_params
+    params.require(:stock_list_item).permit(:stock_id)
+  end
+
+  def set_list_tail!
+    @list = List.find(@stock_list_item.list_id)
+    if @list.list_head
+      @stock_list_item_last_link = StockListItem.find(list_head)
+      while(@stock_list_item_last_link.next_stock_list_id) do
+        @stock_list_item_last_link = StockListItem.find(next_stock_list_id)
+      end
+      @stock_list_item_last_link.next_stock_list_id = @stock_list_item.id
+      @stock_list_item_last_link.save
+    else
+      @list.list_head = @stock_list_item.id
+      @list.save
+    end
+    # run through linked list until next_stock_list_id = nil
+    # set that next_stock_list_id = @stock_list_item.id
   end
 end
