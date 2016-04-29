13f550cdabcf85dc389abe43fd91299e812b800b
Gage <gagesnewman@gmail.com>
2016-04-29 12:14:18 -0700

    adds real data sparkline to StocksIndex

--- frontend/components/stock_list_item.jsx
+++ frontend/components/stock_list_item.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var Sparklines = require('react-sparklines').Sparklines;
+var SparklinesLine = require('react-sparklines').SparklinesLine;
 var ClientActions = require('./../actions/client_actions'),
     StockPriceStore = require('./../stores/stock_price_store');
 
@@ -25,11 +27,23 @@ var StockListItem = React.createClass({
 
   render: function(){
     debugger;
+
+    if(this.props.item){
+      var historicalPriceData = this.props.item.historical_data.map(function(datum){
+                                  return datum.close
+                                });
+    } else {
+      var historicalPriceData = [];
+    }
+
+
     return (
       <div className="stock-list-item" onClick={this.delete}>
         {this.props.item.id}
         {this.props.item.ticker_symbol}
-        ~~~~~~
+        <Sparklines data={historicalPriceData} limit={30} width={90} height={20} margin={5}>
+          <SparklinesLine style={{stroke: "21ce99", fill: "none"}} />
+        </Sparklines>
         <button>${this.props.item.price}</button>
       </div>
     );
