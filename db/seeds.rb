eb75eef423143ff0940b107c70e0347a4048d1aa
Gage <gagesnewman@gmail.com>
2016-05-01 15:53:16 -0700

    adds basic stock seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+stocks = [
+"XOM",
+"GE",
+"MSFT",
+"BP",
+"C",
+"PG",
+"WMT",
+"PFE",
+"HBC",
+"TM",
+"JNJ",
+"BAC",
+"AIG",
+"TOT",
+"NVS",
+"MO",
+"GSK",
+"MTU",
+"JPM",
+"RDS/A",
+"CVX",
+"SNY",
+"VOD",
+"INTC",
+"IBM",
+"E",
+"CSCO",
+"BRK/A",
+"UBS",
+"WFC",
+"T",
+"RDS/B",
+"KO",
+"CHL",
+"PEP",
+"VZ",
+"COP",
+"DNA",
+"AMGN",
+"STD",
+"HPQ",
+"GOOG",
+"HD",
+"WB",
+"SI",
+"NOK",
+"UNH",
+"TWX",
+"QCOM",
+"ING"
+]
+
+stocks.each do |stock|
+  Stock.create(stock)
+end
