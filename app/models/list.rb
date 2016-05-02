99b9642f03982fc36a0ed7fd999ec74efe362446
Gage <gagesnewman@gmail.com>
2016-05-01 22:48:15 -0700

    initial list after login is now five items long

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,16 +5,32 @@ class List < ActiveRecord::Base
   has_many :stock_list_items
 
   def populate_self!
-    #pre-fill list with some stocks
+    #pre-fill list with the first 5 stocks
+
     @current_stock_item = StockListItem.create(
                             list_id: self.id,
                             stock_id: Stock.first.id)
     self.list_head = @current_stock_item.id
-    @current_stock_item.next_stock_list_id = StockListItem.create(
-                                               list_id: self.id,
-                                               stock_id: Stock.last.id).id
+
     self.save
-    @current_stock_item.save
+
+    stocks = Stock.limit(5)
+
+    4.times do |idx|
+
+      next_stock = StockListItem.create(list_id: self.id,
+                                        stock_id: stocks[idx + 1].id)
+
+      @current_stock_item.next_stock_list_id = next_stock.id
+
+      @current_stock_item.save
+
+      @current_stock_item = next_stock
+
+    end
+
+    # list is now created, and there are 5 StockListItems in its linked list
+
   end
 
   def depopulate_self!
