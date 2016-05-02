6db40e461139e91bbc49b7f44082766365991e43
Gage <gagesnewman@gmail.com>
2016-05-02 15:50:00 -0700

    styles login page and adds logo

--- frontend/components/login_form.jsx
+++ frontend/components/login_form.jsx
@@ -59,6 +59,35 @@ var LoginForm = React.createClass({
   },
 
   render: function(){
+    var modalStyle = {
+      overlay : {
+        position        : 'fixed',
+        top             : 0,
+        left            : 0,
+        right           : 0,
+        bottom          : 0,
+        backgroundColor : 'rgba(255, 255, 255, 0.75)',
+        zIndex          : 10
+      },
+      content : {
+        position        : 'fixed',
+        margin          : '0 auto',
+        top             : '100px',
+        bottom          : '100px',
+        border          : '1px solid #ccc',
+        padding         : '20px',
+        zIndex          : 11,
+        height          : "500px",
+        width           : "500px",
+        // minHeight       : "400px",
+        // minWidth        : "350px",
+        display         : "flex",
+        alignItems      : "center",
+        justifyContent  : "center"
+      }
+    };
+
+
     var content;
     if(this.props.currentUser){
       content = (
@@ -70,22 +99,28 @@ var LoginForm = React.createClass({
           <button onClick={this.openModal}>Sign Up</button>
           <Modal
             isOpen={this.state.modal}
-            onRequestClose={this.closeModal}>
+            onRequestClose={this.closeModal}
+            style={modalStyle}>
 
             {this.props.userErrors ? this.props.userErrors : ""}
-              <h1>Sign Up</h1>
-              <form onSubmit={this.signup}>
-                <label>Username
-                  <input type="text" valueLink={this.linkState('username')} />
-                </label>
-                <label>Password
-                  <input type="password" valueLink={this.linkState('password')} />
-                </label>
-                <input type="submit" value="Sign Up" />
-              </form>
-              <p>Actually, I have an account.
-                <a onClick={this.toggleSignup}>Log in</a>.
-              </p>
+              <div className="signup">
+                <h1>Sign Up</h1>
+                <form onSubmit={this.signup}>
+                  <input
+                    type="text"
+                    valueLink={this.linkState('username')}
+                    placeholder="Username"/>
+                  <input
+                    type="password"
+                    valueLink={this.linkState('password')}
+                    placeholder="Password"/>
+                  <input type="submit" value="Sign Up" />
+                </form>
+                <h4>Actually, I have an account.&nbsp;
+                  <a onClick={this.toggleSignup}>Log in</a>.
+                </h4>
+                <button className="guest-login" onClick={this.guestLogin}>Log in as Guest</button>
+              </div>
           </Modal>
         </div>
       );
@@ -95,23 +130,28 @@ var LoginForm = React.createClass({
           <button onClick={this.openModal}>Log In</button>
             <Modal
               isOpen={this.state.modal}
-              onRequestClose={this.closeModal}>
+              onRequestClose={this.closeModal}
+              style={modalStyle}>
 
               {this.props.userErrors ? this.props.userErrors : ""}
-              <h1>Log In</h1>
-              <form onSubmit={this.login}>
-                <label>Username
-                  <input type="text" valueLink={this.linkState('username')} />
-                </label>
-                <label>Password
-                  <input type="password" valueLink={this.linkState('password')} />
-                </label>
-                <input type="submit" value="Let's Go!" />
-              </form>
-              <p>Actually, I don't have an account.
-                <a onClick={this.toggleSignup}>Sign up</a>.
-              </p>
-              <button className="guest-login" onClick={this.guestLogin}>Log in as Guest</button>
+              <div className="login">
+                <h1>Log In</h1>
+                <form onSubmit={this.login}>
+                  <input
+                    type="text"
+                    valueLink={this.linkState('username')}
+                    placeholder="Username"/>
+                  <input
+                    type="password"
+                    valueLink={this.linkState('password')}
+                    placeholder="Password"/>
+                  <input type="submit" value="Let's Go!" />
+                </form>
+                <h4>Actually, I don't have an account.&nbsp;
+                  <a onClick={this.toggleSignup}>Sign up</a>.
+                </h4>
+                <button className="guest-login" onClick={this.guestLogin}>Log in as Guest</button>
+              </div>
           </Modal>
         </div>
       );
--- frontend/components/nav.jsx
+++ frontend/components/nav.jsx
@@ -1,14 +1,20 @@
-var React = require('react');
+var React = require('react'),
+    HashHistory = require('react-router').hashHistory;
 var LoginForm = require('./login_form');
 
 var Nav = React.createClass({
   render: function(){
     return (
       <nav className="nav">
-        <div className="logo">LOGO</div>
+        <div className="logo" onClick={this.logoClick}></div>
         <LoginForm userErrors={this.props.userErrors} currentUser={this.props.currentUser} />
       </nav>
     );
+  },
+
+  logoClick: function(e){
+    e.preventDefault();
+    HashHistory.push("/");
   }
 });
 
