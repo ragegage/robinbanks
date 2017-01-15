b3336b4eeb5f64d851bbf07ec6239c35a9e75baa
Gage <gagesnewman@gmail.com>
2017-01-15 00:21:45 -0800

    set user-agent header on request to get past API blockage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,11 +38,12 @@ class ApplicationController < ActionController::Base
             .concat("/quote?format=json")
 
 
-    response = RestClient.get current_price_url, {accept: "application/json;charset=UTF-8"}#, :user_agent => 'User-Agent', 'Mozilla/5.0 (Linux; Android 6.0; MotoE2(4G-LTE) Build/MPI24.65-39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.81 Mobile Safari/537.36')
+    # response = RestClient.get current_price_url, {accept: "application/json\;charset=UTF-8", user_agent: 'Mozilla/5.0 (Linux\; Android 6.0\; MotoE2(4G-LTE) Build/MPI24.65-39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.81 Mobile Safari/537.36'}
+    response = RestClient.get current_price_url, {accept: "application/json;charset=UTF-8", user_agent: 'Mozilla/5.0 (Linux; Android 6.0; MotoE2(4G-LTE) Build/MPI24.65-39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.81 Mobile Safari/537.36'}
 
     # response's content type is "application/json;charset=UTF-8"
 
-    # debugger
+    debugger
 
     current_price_string = response.to_s
 
